a72dc21eb58c6ab2df70f930191b93d114b11ed9
Antioch Sanders <2018asanders@tjhsst.edu>
2016-01-27 09:18:45 -0500

    Remove Unused Args From MenuButton.java

--- gui/MenuButton.java
+++ gui/MenuButton.java
@@ -7,12 +7,19 @@ import java.util.Random;
 import gfm.util.Vec2;
 
 public class MenuButton extends BasicButton {
-   public MenuButton(ActionListener listener, String text, Vec2 position, Vec2 size, int gameWidth, int gameHeight) {
+   public MenuButton(ActionListener listener, String text, Vec2 position, Vec2 size) {
       super(listener, text, Color.black, Color.black, null, position, size);
-      Random r = new Random();
-      int base = 100;
-      int rand = 255 - base;
-      setBodyColor(new Color(base + r.nextInt(rand), base + r.nextInt(rand), base + r.nextInt(rand)));
+      setRandomBodyColor();
       setTextColor(new Color(100, 100, 100));
+   }
+
+   public void setRandomBodyColor() {
+      Random rand = new Random();
+      int base = 100;
+      int range = 255 - base;
+      int red = base + rand.nextInt(range + 1);
+      int green = base + rand.nextInt(range + 1);
+      int blue = base + rand.nextInt(range + 1);
+      setBodyColor(new Color(red, green, blue));
    }
 }
