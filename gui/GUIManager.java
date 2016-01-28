78f99bd18ba06901db3b7c958b79d864d53256f7
Antioch Sanders <2018asanders@tjhsst.edu>
2016-01-27 22:00:00 -0500

    Change Lots in GUIManager.java
    -myGUIComponents added
    ...getters/setters added
    ...updated/drawn
    ...listen for events
    -sound names moved to Button
    -minor reorganization

--- gui/GUIManager.java
+++ gui/GUIManager.java
@@ -5,99 +5,169 @@ import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseWheelEvent;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import gfm.Game;
 import gfm.Macro;
 
+// Note, not everything is treated fairly,
+// can only disable buttons, only they make
+// sound, etc.
+
 public class GUIManager implements Macro {
    private Game myGame;
 
-   /**
-    *
-    */
    private ArrayList<Button> myButtons;
+   private ArrayList<GUIComponent> myGUIComponents;
    private boolean myDisabled;
-   private String myHoverSound;
-   private String myClickSound;
 
    public GUIManager(Game game) {
       myGame = game;
       myButtons = new ArrayList<Button>();
+      myGUIComponents = new ArrayList<GUIComponent>();
       myDisabled = false;
-      myHoverSound = null;
-      myClickSound = null;
    }
 
-   /**
-    * @param pen
-    */
    @Override
    public void draw(Graphics pen) {
       for ( int i = 0; i < myButtons.size(); i++ ) {
          Button button = myButtons.get(i);
          if (button.getMouseHovering()) {
             button.drawHovered(pen);
-         } 
-         else {
+         } else {
             button.draw(pen);
          }
       }
+
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.draw(pen);
+      }
    }
 
-   /**
-    * @param event
-    */
+   @Override
+   public void update() {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.update();
+      }
+   }
+
    @Override
    public void mousePressed(MouseEvent event) {
       if ( myDisabled || event.getButton() != MouseEvent.BUTTON1 ) {
          return;
       }
+
       for ( int i = 0; i < myButtons.size(); i++ ) {
          Button button = myButtons.get(i);
          if ( button.collidesPoint(event.getX(), event.getY()) ) {
-            if ( myClickSound != null) {
-               myGame.getSoundManager().play(myClickSound);
+            String clickSound = button.getClickSound();
+            if ( clickSound != null) {
+               myGame.getSoundManager().play(clickSound);
             }
             button.doAction();
          }
       }
+
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mousePressed(event);
+      }
    }
 
-   /**
-    * @param event
-    */
    @Override
    public void mouseMoved(MouseEvent event) {
-      if ( myDisabled ) { 
-         return; }
+      if ( myDisabled ) {
+         return;
+      }
+
       for ( int i = 0; i < myButtons.size(); i++ ) {
          Button button = myButtons.get(i);
          if ( button.collidesPoint(event.getX(), event.getY())) {
             if ( button.getMouseHovering() == false ) {
-               if ( myHoverSound != null) {
-                  myGame.getSoundManager().play(myHoverSound);
+               String hoverSound = button.getHoverSound();
+               if ( hoverSound != null) {
+                  myGame.getSoundManager().play(hoverSound);
                }
                button.setMouseHovering(true);
             }
-         } 
-         else {
+         } else {
             button.setMouseHovering(false);
          }
       }
+
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseMoved(event);
+      }
    }
 
-   /**
-    *
-    */
+   @Override
+   public void keyPressed(KeyEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.keyPressed(event);
+      }
+   }
+
+   @Override
+   public void keyReleased(KeyEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.keyReleased(event);
+      }
+   }
+
+   @Override
+   public void keyTyped(KeyEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.keyTyped(event);
+      }
+   }
+
+   @Override
+   public void mouseClicked(MouseEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseClicked(event);
+      }
+   }
+
+   @Override
+   public void mouseDragged(MouseEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseDragged(event);
+      }
+   }
+
+   @Override
+   public void mouseEntered(MouseEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseEntered(event);
+      }
+   }
+
+   @Override
+   public void mouseExited(MouseEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseExited(event);
+      }
+   }
+
+   @Override
+   public void mouseReleased(MouseEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseReleased(event);
+      }
+   }
+
+   @Override
+   public void mouseWheelMoved(MouseWheelEvent event) {
+      for ( GUIComponent comp : myGUIComponents ) {
+         comp.mouseWheelMoved(event);
+      }
+   }
+
    public void resetInputs() {
       for (Button button: myButtons) {
          button.setMouseHovering(false);
       }
    }
 
-   /**
-    *
-    */
    public void disable() {
       myDisabled = true;
       for ( int i = 0; i < myButtons.size(); i++ ) {
@@ -105,9 +175,6 @@ public class GUIManager implements Macro {
       }
    }
 
-   /**
-    *
-    */
    public void enable() {
       myDisabled = false;
    }
@@ -116,11 +183,8 @@ public class GUIManager implements Macro {
       myButtons = new ArrayList<Button>();
    }
 
-   /**
-    * @param button
-    */
    public void addButton(Button button) {
-      button.setGame(myGame);
+      button.setGUIManager(this);
       myButtons.add(button);
    }
 
@@ -128,49 +192,19 @@ public class GUIManager implements Macro {
       myButtons.remove(button);
    }
 
-   public void setHoverSound(String sound) {
-      myHoverSound = sound;
+   public void addGUIComponent(GUIComponent toAdd) {
+      myGUIComponents.add(toAdd);
    }
 
-   public void setClickSound(String sound) {
-      myClickSound = sound;
+   public void removeGUIComponent(GUIComponent toRemove) {
+      myGUIComponents.remove(toRemove);
    }
 
-   public ArrayList<Button> getButtons() {
-      return myButtons;
+   public void deleteAllComponents() {
+      myGUIComponents = new ArrayList<GUIComponent>();
    }
 
-   public Game getGame() {
-      return myGame;
-   }
-
-   @Override
-   public void update() { }
-
-   @Override
-   public void keyPressed(KeyEvent event) { }
-
-   @Override
-   public void keyReleased(KeyEvent event) { }
-
-   @Override
-   public void keyTyped(KeyEvent event) { }
-
-   @Override
-   public void mouseClicked(MouseEvent event) { }
-
-   @Override
-   public void mouseDragged(MouseEvent event) { }
-
-   @Override
-   public void mouseEntered(MouseEvent event) { }
-
-   @Override
-   public void mouseExited(MouseEvent event) { }
-
-   @Override
-   public void mouseReleased(MouseEvent event) { }
-
-   @Override
-   public void mouseWheelMoved(MouseWheelEvent event) { }
+   public Game getGame() { return myGame; }
+   public ArrayList<Button> getButtons() { return myButtons; }
+   public Collection<GUIComponent> getGUIComponents() { return myGUIComponents; }
 }
