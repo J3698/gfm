9384e5f70595c1587862e5b32f62365b2bf71294
J3698 <antiochsanders@gmail.com>
2016-03-10 11:56:24 -0500

    Update ImageManager.java
    -addImage further worked on

--- ImageManager.java
+++ ImageManager.java
@@ -1,5 +1,12 @@
 package gfm;
 
+import java.util.HashMap;
+import java.util.Collection;
+import java.net.URL;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
 class ImageManager {
    private static ImageManager mySingleton;
    public static ImageManager getSingleton() {
@@ -12,14 +19,22 @@ class ImageManager {
    private HashMap<String, Image> myImages;
 
    public ImageManager(int initCapacity) {
-      myImages = new HashMap<String, Image>(capacity);
+      myImages = new HashMap<String, Image>(initCapacity);
    }
    public ImageManager() {
       myImages = new HashMap<String, Image>();
    }
 
    public boolean addImage(String path) {
-      
+      URL url = getClass().getResource(path);
+      if ( url == null ) {
+         return false;
+      }
+
+
+      ImageIcon icon = new ImageIcon(url);
+      // fix errors if URL is wrong - up to
+      // here, we know that URL is not null, thus it exists
    }
 
    public boolean addImage(String path, String name) {
@@ -27,6 +42,7 @@ class ImageManager {
    }
 
    public Image getImage(String name) {
+      return myImages.get(name);
    }
 
    public void clear() {
@@ -35,7 +51,7 @@ class ImageManager {
    public int size() {
       return myImages.size();
    }
-   public Collection<V> getImages() {
+   public Collection<Image> getImages() {
       return myImages.values();
    }
 }
